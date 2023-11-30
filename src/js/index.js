3cf6137d18706c1b9a670b3fb5987ec5bd4c6397
DmGle <dimon.gleda1@gmail.com>
2023-11-30 19:03:23 +0200

    fix_scroll

--- src/js/index.js
+++ src/js/index.js
@@ -4,46 +4,11 @@ import { createImageCard } from './gallery';
 
 document.addEventListener('DOMContentLoaded', function () {
   const gallery = document.querySelector('.gallery');
-  const loadMoreBtn = document.querySelector('.load-more');
   let currentPage = 1;
-  const searchForm = document.getElementById('search-form');
-
-  if (searchForm) {
-    searchForm.addEventListener('submit', async function (event) {
-      event.preventDefault();
-      const searchQuery = event.target.elements.searchQuery.value;
-
-      try {
-        const { images, totalHits } = await fetchImages(searchQuery, currentPage);
-        renderGallery(images);
-        handleLoadMoreBtn(totalHits);
-      } catch (error) {
-        console.error('Error fetching images:', error.message);
-        Notiflix.Notify.failure('Error fetching images. Please try again.');
-        clearGallery();
-      }
-    });
-  }
-
-  if (loadMoreBtn) {
-    loadMoreBtn.addEventListener('click', async function () {
-      currentPage++;
-      const searchQuery = searchForm.elements.searchQuery.value;
-
-      try {
-        const { images, totalHits } = await fetchImages(searchQuery, currentPage);
-        renderGallery(images);
-        handleLoadMoreBtn(totalHits);
-      } catch (error) {
-        console.error('Error fetching images:', error.message);
-        Notiflix.Notify.failure('Error fetching images. Please try again.');
-        clearGallery();
-      }
-    });
-  }
+  const imagesPerPage = 40;
+  let loading = false; // Додана змінна для уникнення повторних запитів
 
   function renderGallery(images) {
-    gallery.innerHTML = '';
     if (images.length > 0) {
       images.forEach(image => {
         const card = createImageCard(image);
@@ -54,7 +19,27 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
+  async function loadMoreImages() {
+    if (loading) return;
+    loading = true;
+  
+    const searchQuery = searchForm.elements.searchQuery.value;
+  
+    try {
+      const { images, totalHits } = await fetchImages(searchQuery, currentPage);
+      renderGallery(images);
+      handleLoadMoreBtn(totalHits);
+      loading = false;
+    } catch (error) {
+      console.error('Error fetching images:', error.message);
+      Notiflix.Notify.failure('Error fetching images. Please try again.');
+      loading = false;
+    }
+  }
+
   function handleLoadMoreBtn(totalHits) {
+    const loadMoreBtn = document.querySelector('.load-more');
+
     if (loadMoreBtn) {
       loadMoreBtn.style.display = totalHits > currentPage * imagesPerPage ? 'block' : 'none';
       if (totalHits <= currentPage * imagesPerPage) {
@@ -64,12 +49,30 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   }
 
-  function clearGallery() {
-    gallery.innerHTML = '';
-  }
-
   function showNoResultsMessage() {
     const message = 'Sorry, there are no images matching your search query. Please try again.';
     Notiflix.Notify.failure(message);
+  }
+
+  window.addEventListener('scroll', function () {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = window.scrollY;
+    const clientHeight = document.documentElement.clientHeight;
+
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
+      currentPage++;
+      loadMoreImages();
+    }
+  });
+
+  const searchForm = document.getElementById('search-form');
+
+  if (searchForm) {
+    searchForm.addEventListener('submit', function (event) {
+      event.preventDefault();
+      currentPage = 1;
+      gallery.innerHTML = ''; // Очищаємо галерею при новому пошуку
+      loadMoreImages();
+    });
   }
 });
